246213ee2c3b61e4de7fd453b5f7c4dcd674f816
200430116058 <yoganandimihir@gmail.com>
2025-02-13 16:48:13 +0530

    Product section complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,11 @@ import { CarouselDarkVariantExample } from '../Component/Test';
 import { motion } from 'framer-motion';
 import VideoCarousle from '../Component/VideoCarousle';
 import { style } from 'framer-motion/client';
-
-
+import CardComponent from '../Component/CardComponent';
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaThreads } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
 
 export default function index() {
     const [isHeaderVisible, setIsHeaderVisible] = useState(false);
@@ -154,6 +157,19 @@ export default function index() {
                 <div className={styles.VideoSlider}>
                     <VideoCarousle />
                 </div>
+                <div className={styles.cardComponent}>
+                    <div className={styles.heading}>
+                        <h1>Our Best Seller</h1>
+                    </div>
+                    <div className={styles.cardInner}>
+
+                        <CardComponent src={"/Images/Round-Glasses.webp"} name={"Round Glasses"} price={"$28.00"} />
+                        <CardComponent src={"/Images/glasses2.webp"} name={"Rectangle SunGlasses"} price={"$59.00"} />
+                        <CardComponent src={"/Images/glasses3.webp"} name={"Rectangle-SunGlasses-S"} price={"$15.00"} />
+                        <CardComponent src={"/Images/glasses4.webp"} name={"Premium-SunGlasses-S"} price={"$20.00"} />
+
+                    </div>
+                </div>
                 <div className={styles.footer}>
                     <div className={styles.footerInner}>
                         <div className={styles.left}>
@@ -163,9 +179,10 @@ export default function index() {
                                 </div>
                                 <p>Sophisticated simplicity for the independent mind</p>
                                 <div className={styles.icons}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" /></svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+                                    <FaXTwitter width={24} height={24} />
+                                    <FaInstagram />
+                                    <FaThreads />
+                                    <FaFacebookF />
                                 </div>
                             </div>
                         </div>
