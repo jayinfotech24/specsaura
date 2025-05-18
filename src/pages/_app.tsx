1b117acb2687a6631a4e3fd8dc5184310c78a19a
Mihir2003 <yoganandimihir@gmail.com>
2025-05-18 15:11:33 +0530

    favicon icon updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
   useEffect(() => {
-         
+
     const startLoading = () => { setIsLoading(true) }
     const stopLoading = () => {
       setIsLoading(false);
@@ -29,16 +29,16 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
     return () => {
-        router.events.off("routeChangeStart", startLoading);
-    router.events.off("routeChangeComplete", stopLoading);
-    router.events.off("routeChangeError", stopLoading);
+      router.events.off("routeChangeStart", startLoading);
+      router.events.off("routeChangeComplete", stopLoading);
+      router.events.off("routeChangeError", stopLoading);
     }
   }, [router.events])
-  
+
 
 
   return (
-    
+
     <>
       {
         isLoading && (
@@ -46,25 +46,26 @@ export default function App({ Component, pageProps }: AppProps) {
         )
 
       }
-   <Provider store={store}>
-         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" 
-          rel="stylesheet" 
+      <Provider store={store}>
+        <Head>
+          <title>SpecsAura</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
           />
-          
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"  />
-<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Marcellus&display=swap" rel="stylesheet"></link>
-      </Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Marcellus&display=swap" rel="stylesheet"></link>
+        </Head>
         <Component {...pageProps} />
-       
- </Provider>
+
+      </Provider>
     </>
   );
 }
